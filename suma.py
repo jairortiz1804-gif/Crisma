03e384a105612aa9f8cfd76f9a8e9325a874117f
Jair Hernández <jairortiz1804@gmail.com>
2026-04-23 17:45:51 -0600

    Agrego programa de suma

---
+++ suma.py
@@ -0,0 +1,8 @@
+# Programa para sumar dos números
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+resultado = num1 + num2
+
+print(f"El resultado es: {resultado}")
